f17c865e8502b45aac13ecde1c5d13ccf834f0ae
novafacing <rowanbhart@gmail.com>
2022-11-05 12:45:37 -0400

    Fix sed stmt

--- bin/set.sh
+++ bin/set.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find . -type d -maxdepth 1 -mindepth 1 | xargs -i bash -c \
-'qemu_prog=$(printf {} | cut -d'/' -f2); qemu_func=$(printf $qemu_prog | sed "s/-/_/g"); echo $qemu_prog; echo $qemu_func; echo "memfd-exec = \"0.1.4\"" >> {}/Cargo.toml; cat main.rs.template | sed "s/QEMU_FUNCNAME/$qemu_func/g" | sed "s/QEMU_PROGNAME/$qemu_prog" > {}/src/main.rs'
+'qemu_prog=$(printf {} | cut -d'/' -f2); qemu_func=$(printf $qemu_prog | sed "s/-/_/g"); echo $qemu_prog; echo $qemu_func; echo "memfd-exec = \"0.1.4\"" >> {}/Cargo.toml; cat main.rs.template | sed "s/QEMU_FUNCNAME/$qemu_func/g" | sed "s/QEMU_PROGNAME/$qemu_prog/g" > {}/src/main.rs'
