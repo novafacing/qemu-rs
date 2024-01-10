c5c4c5314905fcec56573e7996c88de034835b92
novafacing <rowanbhart@gmail.com>
2024-01-10 10:06:07 -0800

    Fix a couple issues

--- qemu-plugin/src/win_link_hook/mod.rs
+++ qemu-plugin/src/win_link_hook/mod.rs
@@ -6,8 +6,8 @@ use windows::Win32::System::LibraryLoader::GetModuleHandleExA;
 use windows::Win32::System::WindowsProgramming::DELAYLOAD_INFO;
 
 /// The helper function for linker-supported delayed loading which is what actually
-/// loads the DLL at runtime. 
-type DelayHook = unsafe extern "C" fn(dli_notify: u32, pdli: DELAYLOAD_INFO) -> HMODULE;
+/// loads the DLL at runtime.
+type DelayHook = unsafe extern "C" fn(dli_notify: DliNofiy, pdli: DELAYLOAD_INFO) -> HMODULE;
 
 #[no_mangle]
 /// Helper function invoked when failures occur in delay linking (as opposed to
@@ -33,23 +33,24 @@ enum DliNotify {
     /// Called after all processing is done, no bypass possible at this point except by
     /// longjmp()/throw()/RaiseException.
     DliNoteEndProcessing,
-};
+}
 
 /// Helper function invoked when notifications or failures occur in delay linking
-/// 
+///
 /// # Arguments
-/// 
-/// * `dli_notify` - The 
+///
+/// * `dli_notify` - The
 extern "C" fn delaylink_hook(dli_notify: DliNotify, pdli: DELAYLOAD_INFO) -> HMODULE {
-    if let DliNofiy::DliFailLoadLib = dli_notify {
+    if let DliNotify::DliFailLoadLib = dli_notify {
         // SAFETY: Conversion of `PCSTR` to String is not safe because it involves an unchecked
-        // nul-byte dependent `strcpy`. In this instance, it is safe because 
+        // nul-byte dependent `strcpy`. In this instance, it is safe because
         let name = unsafe { pdli.TargetDllName.to_string() }.unwrap_or_default();
         let mut module = HMODULE::default();
 
         // NOTE: QEMU executables on windows are named qemu-system.*.exe
         if name.starts_with("qemu") {
-            /// SAFETY: 
+            // SAFETY: Getting the module handle for NULL is safe and does not dereference any
+            // pointers except to write the `module` argument which we know is alive here.
             match unsafe { GetModuleHandleExA(0, PCSTR::null(), &mut module as *mut HMODULE) } {
                 Ok(_) => return module,
                 Err(e) => panic!("Failed to open QEMU module: {e:?}"),
