f6e4c5a49bcd81f4ae96663cfd7660e81a72b236
novafacing <rowanbhart@gmail.com>
2024-06-02 18:11:51 -0700

    Do not use pushd and fix versioning for tiny plugin

--- plugins/tiny/src/lib.rs
+++ plugins/tiny/src/lib.rs
@@ -2,13 +2,15 @@ use anyhow::{anyhow, Result};
 use ctor::ctor;
 use qemu_plugin::{
     plugin::{HasCallbacks, Plugin, Register, PLUGIN},
-    PluginId, TranslationBlock, VCPUIndex, qemu_plugin_get_registers,
-    RegisterDescriptor,
+    PluginId, TranslationBlock,
 };
+#[cfg(any(feature = "plugin-api-v2", feature = "plugin-api-v3"))]
+use qemu_plugin::{qemu_plugin_get_registers, RegisterDescriptor, VCPUIndex};
 use std::sync::Mutex;
 
 #[derive(Default)]
 struct TinyTrace {
+    #[cfg(any(feature = "plugin-api-v2", feature = "plugin-api-v3"))]
     registers: Vec<RegisterDescriptor<'static>>,
 }
 
@@ -16,6 +18,7 @@ impl Plugin for TinyTrace {}
 impl Register for TinyTrace {}
 
 impl HasCallbacks for TinyTrace {
+    #[cfg(any(feature = "plugin-api-v2", feature = "plugin-api-v3"))]
     fn on_vcpu_init(&mut self, _id: PluginId, _vcpu_id: VCPUIndex) -> Result<()> {
         self.registers = qemu_plugin_get_registers()?;
         Ok(())
@@ -25,19 +28,24 @@ impl HasCallbacks for TinyTrace {
         _id: PluginId,
         tb: TranslationBlock,
     ) -> Result<()> {
+        #[cfg(any(feature = "plugin-api-v2", feature = "plugin-api-v3"))]
         let registers = self.registers.clone();
+
         tb.instructions().try_for_each(|insn| {
             println!("{:08x}: {}", insn.vaddr(), insn.disas()?);
-            for register in &registers {
-                let value = register.read()?;
-                println!("    {}: {:?}", register.name, value);
+
+            #[cfg(any(feature = "plugin-api-v2", feature = "plugin-api-v3"))]
+            {
+                for register in &registers {
+                    let value = register.read()?;
+                    println!("    {}: {:?}", register.name, value);
+                }
             }
 
             Ok(())
         })
     }
 }
-
 
 #[ctor]
 fn init() {
