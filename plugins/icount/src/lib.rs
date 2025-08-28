7f9d4e119a79a77c7ae2568e466eea2324d82d33
Rowan Hart <rowanbhart@gmail.com>
2025-08-27 21:50:42 -0700

    More updates/lifetime stuff/reorganization
    Signed-off-by: Rowan Hart <rowanbhart@gmail.com>

---
+++ plugins/icount/src/lib.rs
@@ -0,0 +1,41 @@
+use anyhow::Result;
+use ctor::ctor;
+use qemu_plugin::{
+    plugin::{init_plugin, HasCallbacks, Register},
+    PluginId, TranslationBlock,
+};
+#[cfg(not(feature = "plugin-api-v1"))]
+use qemu_plugin::{qemu_plugin_get_registers, RegisterDescriptor, VCPUIndex};
+
+#[derive(Default)]
+struct ICount;
+
+impl Register for ICount {}
+
+impl HasCallbacks for ICount {
+    fn on_vcpu_init(&mut self, _id: PluginId, _vcpu_id: VCPUIndex) -> Result<()> {
+        Ok(())
+    }
+
+    fn on_translation_block_translate(
+        &mut self,
+        _id: PluginId,
+        tb: TranslationBlock,
+    ) -> Result<()> {
+        tb.instructions().try_for_each(|insn| {
+            let vaddr = insn.vaddr();
+            let disas = insn.disas()?;
+
+            insn.register_execute_callback(move |_idx| {
+                println!("{:08x}: {}", vaddr, disas);
+            });
+
+            Ok(())
+        })
+    }
+}
+
+#[ctor]
+fn init() {
+    init_plugin(ICount::default()).expect("Failed to initialize plugin");
+}
